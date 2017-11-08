8a652542a5d0f36e2a307fe62d1a46b0c7db7c49
Matthew Vastarelli <mvast1@unh.newhaven.edu>
2017-11-08 17:33:31 -0500

    Reverse int

---
+++ Reverse Int/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+    int revInt = 120;
+    string revStr = "";
+    
+    revStr = to_string(revInt);
+    
+    if (revInt < 0) {
+        if ((revInt%10) == 0) {
+            revStr.erase(0,1);
+            revStr.pop_back();
+            reverse(revStr.begin(), revStr.end());
+            cout << "-" << revStr << endl;
+        }
+        else {
+            revStr.erase(0,1);
+            reverse(revStr.begin(), revStr.end());
+            cout << "-" << revStr << endl;
+        }
+    }
+    if (revInt >= 0) {
+        if ((revInt%10) == 0) {
+            revStr.pop_back();
+            reverse(revStr.begin(), revStr.end());
+            cout << revStr << endl;
+        }
+        else{
+            reverse(revStr.begin(), revStr.end());
+            cout << revStr << endl;
+        }
+    }
+    
+    return 0;
+}

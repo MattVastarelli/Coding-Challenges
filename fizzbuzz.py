620bcadde2c3e34cce650a60098ceec78bc69c07
Matthew Vastarelli <30515659+MattVastarelli@users.noreply.github.com>
2018-05-29 10:14:14 -0400

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,15 @@
+def FizzBuzz(value):
+    if value % 5 == 0 and value % 3 == 0:
+        return "FizzBuzz"
+    if value % 5 == 0:
+        return "Fizz"
+    if value % 3 == 0:
+        return "Buzz"
+    else:
+        return value
 
+
+print("FizzBuzz")
+
+for x in range(100):
+    print(FizzBuzz(x))
